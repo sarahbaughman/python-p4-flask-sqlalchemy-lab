e837e56bc62374e884450710250f8e45b5e68729
Sarah Baughman <sarahbaughman92@gmail.com>
2023-05-24 18:34:00 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
@@ -14,20 +12,87 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 @app.route('/')
-def home():
+def index():
     return '<h1>Zoo app</h1>'
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    response_body = f'''
+        <ul>ID: {animal.id}</ul>
+        <ul>Name: {animal.name}</ul>
+        <ul>Species: {animal.species}</ul>
+        <ul>Zookeeper: {animal.zookeeper.name}</ul>
+        <ul>Enclosure: {animal.enclosure.environment}</ul>
+    '''
+    # response_body += f'<ul>ID: {animal.id}</ul>'
+    # response_body += f'<ul>Name: {animal.name}</ul>'
+    # response_body += f'<ul>Species: {animal.species}</ul>'
+    # response_body += f'<ul>Zookeeper: {animal.zookeeper.name}</ul>'
+    # response_body += f'<ul>Enclosure: {animal.enclosure.environment}</ul>'
+    
+    response = make_response(response_body, 200)
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+
+    if not zookeeper:
+            response_body = '<h1>404 Error: Zookeeper not found.</h1>'
+            return make_response(response_body, 404)
+    
+    response_body = f'''
+        <ul>ID: {zookeeper.id}</ul>
+        <ul>Name: {zookeeper.name}</ul>
+        <ul>Birthday: {zookeeper.birthday}</ul>
+        '''
+    
+    for animal in zookeeper.animals:
+        response_body += f'<ul>Animal: {animal.name}</ul>'
+
+    # animals = [animal for animal in zookeeper.animals]
+
+    # if not animals:
+    #     response_body = f'<h1>{zookeeper.name} has no animals right now.</h1>'
+    
+    # else: 
+    #     for animal in animals:
+    #         response_body += f'<h1>Animal:{animal.name} </h1>'
+    response = make_response(response_body, 200)
+    
+    return response
+
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+
+    if not enclosure:
+            response_body = '<h1>404 Error: Enclosure not found.</h1>'
+            return make_response(response_body, 404)
+    
+    response_body = f'''
+        <ul>ID: {enclosure.id}</ul>
+        <ul>Environment: {enclosure.environment}</ul>
+        <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>
+        '''
+    
+    for animal in enclosure.animals:
+        response_body += f'<ul>Animal: {animal.name}</ul>'
+
+    
+    # animals = [animal for animal in enclosure.animals]
+
+    # if not animals:
+    #     response_body = f'<h1>{enclosure.environment} has no animals right now.</h1>'
+    
+    # else: 
+    #     for animal in animals:
+    #         response_body += f'<h1>Animal:{animal.name} </h1>'
+    response = make_response(response_body, 200)
+    
+    return response
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
+
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
@@ -11,13 +12,32 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
+
+    animals = db.relationship('Animal', back_populates = 'zookeeper')
+
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+    
+    animals = db.relationship('Animal', back_populates = 'enclosure')
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique = True)
+    species = db.Column(db.String)
+
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
+    zookeeper = db.relationship('Zookeeper', back_populates = 'animals')
+    enclosure = db.relationship('Enclosure', back_populates = 'animals')
+
+
